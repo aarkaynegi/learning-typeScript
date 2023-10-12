4c6de60bc7e8c08d5df3a19dbff5319f924e25a1
aarkaynegi <aarkaynegi2@gmail.com>
2023-10-12 14:25:33 +0530

    clear

--- dist/index.js
+++ dist/index.js
@@ -1,30 +1,19 @@
-// // fetch('https://jsonplaceholder.typicode.com/todos/1')
-// // .then(response => response.json())
-// // .then(json => console.log(json))
-// // fetch('https://jsonplaceholder.typicodsse.com/todos/1')
-// //   .then(response => response.json())
-// //   .then(json => { console.log(json) })
-// //   .catch(err => err.message)
-// //   .then(message => console.log('Wooah', message))
 // interface ILocation { lat: number, long: number }
 // interface IRes {
 //   userId: number
 //   id: number
 //   title: string
 //   completed: boolean
-//   // location: ILocation[]
-//   // location: { lat: number, long: number }[]
-//   // location: Array<{ lat: number, long: number }>
 // }
 // interface IExtendIRes extends IRes {
 //   gender: string
 // }
-// // type Pretty<T> = {
-// //   [K in keyof T]: T[K]
-// // } & {}
-// // function consoleValues(payload: Pretty<IExtendIRes>) {
-// //   console.log('Values', payload)
-// // }
+// type Pretty<T> = {
+//   [K in keyof T]: T[K]
+// } & {}
+// function consoleValues(payload: Pretty<IExtendIRes>) {
+//   console.log('Values', payload)
+// }
 // async function callAPI() {
 //   try {
 //     const res = await fetch('https://jsonplaceholder.typicode.com/todos/1')
